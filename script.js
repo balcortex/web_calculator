41bb81e389baada96da89a832e75a658b1ff456e
Bal Cortex <balcortex@hotmail.com>
2024-01-03 18:14:09 -0600

    feat: support for all operators

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll("button");
 const screen = document.querySelector('div#screen');
 let numCur = '0';
 let numMem = '0';
+let operator = '';
 
 buttons.forEach((button) =>
   button.addEventListener("click", handleButton)
@@ -11,7 +12,7 @@ function handleButton(e) {
   const [, type, value] = e.target.id.split('-');
   // the first part of the id is `button`, we do not need that
   // type -> `num`, `op`, 
-  // value -> the actual value of the button (e.g., 7, 0, 8, etc.)'
+  // value -> the actual value of the button (e.g., 7, 0, 8, etc.)
 
   switch (type) {
     case 'num':
@@ -22,12 +23,26 @@ function handleButton(e) {
     case 'op':
       numMem = screen.innerText; // save the current number on screen to memory
       numCur = '0';  // clear the current number
+      operator = value;
       break;
 
     case 'eq':
-      screen.innerText = +numCur + +numMem;
+      screen.innerText = performOperation(+numMem, +numCur, operator);
       numMem = screen.innerText;
       numCur = '0'; // clear the memory
       break;
+  }
+}
+
+function performOperation(num1, num2, operator) {
+  switch (operator) {
+    case 'div':
+      return num1 / num2;
+    case 'mul':
+      return num1 * num2;
+    case 'dif':
+      return num1 - num2;
+    default:
+      return num1 + num2;
   }
 }
